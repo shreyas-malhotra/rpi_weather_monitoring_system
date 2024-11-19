bc241e3bc5ee1a7e5d98c66c54b8a8bb0c8a132f
Shreyas Malhotra <126407864+shreyas-malhotra@users.noreply.github.com>
2024-11-19 13:50:28 +0530

    Minor updates, 2nd second test run, and adding files for jupyter installation and mq2 sensor.

--- individual_components/dht11.py
+++ individual_components/dht11.py
@@ -1,7 +1,7 @@
 import Adafruit_DHT as dht
 from time import sleep
 DHT_SENSOR = dht.DHT11
-DHT_PIN= 4
+DHT_PIN= 13
 while True:
 	humidity,temprature = dht.read_retry(DHT_SENSOR,DHT_PIN)
 	if humidity is not None and temprature is not None:
