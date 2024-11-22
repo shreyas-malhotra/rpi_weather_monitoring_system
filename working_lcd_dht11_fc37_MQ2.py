5965e442f8f7cfbfefd46343f34f922cb7f06f40
Shreyas Malhotra <126407864+shreyas-malhotra@users.noreply.github.com>
2024-11-22 05:52:28 +0530

    Added code for MQ2 sensor, and tested that it works well after being integrated with the whole project.

--- working_lcd_dht11_fc37_MQ2.py
+++ working_lcd_dht11_fc37_MQ2.py
@@ -3,6 +3,11 @@ import I2C_LCD_driver
 import Adafruit_DHT as dht
 from time import *
 from gpiozero import InputDevice
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+DO_PIN = 13
+GPIO.setup(DO_PIN, GPIO.IN)
 
 mylcd = I2C_LCD_driver.lcd()
 
@@ -16,6 +21,7 @@ while True:
         print("Rain detected.")
     else:
         print("Rain not detected.")
+        
     # DHT11
     humidity,temprature = dht.read_retry(DHT_SENSOR,DHT_PIN)
     if humidity is not None and temprature is not None:
@@ -24,3 +30,15 @@ while True:
         sleep(2)
     else:
         print("Sensor malfunction.")
+        
+    #MQ2    
+    gas_present = GPIO.input(DO_PIN)
+    
+    if gas_present == GPIO.LOW:
+        gas_state = "Gas Present"
+    else:
+        gas_state = "No Gas"
+ 
+    print("Gas State: ", gas_state)
+ 
+    sleep(0.5)
