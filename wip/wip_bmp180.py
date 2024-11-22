a2ea01c2b1e180491d501e20e2a6200d78b48836
Shreyas Malhotra <126407864+shreyas-malhotra@users.noreply.github.com>
2024-11-22 05:55:07 +0530

    Removed code/dependency specifications for components no longer used/required.

--- wip/wip_bmp180.py
+++
@@ -1,5 +0,0 @@
-import Adafruit_BMP.BMP085 as BMP085
-sensor = BMP085.BMP085()
-print('Pressure={0:0.2f} Pa'.format(sensor.read_pressure()))
-print('Altitude={0:0.2f} m'.format(sensor.read_altitude()))
-print('Pressure={0:0.2f} Pa'.format(sensor.read_sealevel_pressure()))      
